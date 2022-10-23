196c1d58c30b3e0f335519525cbe5b9c5faec06e
GreatZue <greatzue@gmail.com>
2022-10-23 15:39:11 -0600

    added axios

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyParser  = require('body-parser')
+const axios = require('axios')
 
 var PORT = process.env.port || 3000
 app.use(express.static('views'))
