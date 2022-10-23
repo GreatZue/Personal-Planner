380e3129cfcc39e5b566ddb48ae564b0c94e91f5
GreatZue <greatzue@gmail.com>
2022-10-23 16:24:54 -0600

    commit

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,12 +1,13 @@
 const axios = require('axios')
+require('dontenv').config();
 
 const url = new URL('https://api.unsplash.com/photos/?client+id=')
 
 async function getPhotos() {
     try {
-        axios.get(url + )
+        axios.get(url + process.env.ACCESS_KEY)
     }
     catch (error) {
-
+        console.log("Could not retreive photos.")
     }
 }
